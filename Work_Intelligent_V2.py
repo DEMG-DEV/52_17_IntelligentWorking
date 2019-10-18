b71906b1a47ca9d7bd39af0d9754aa903c936be3
David Mendez Guardado <demg@outlook.com>
2019-10-18 17:56:03 -0500

    Add files via upload

--- Work_Intelligent_V2.py
+++ Work_Intelligent_V2.py
@@ -1,24 +1,27 @@
 from win10toast import ToastNotifier
 import time
+import datetime
 
 def work_countdown(n=52):
+    print ("INFO " + str(datetime.datetime.now()) + " => StartWorking")
     while n > 0:
         n = n - 1
         time.sleep(60)
         if n == 0:
             toaster = ToastNotifier()
             toaster.show_toast("Rest for 17 minutes!!!","Rest 17 minutes to get focused")
-            print ("Rest for 17 minutes!!!")
+            print ("INFO " + str(datetime.datetime.now()) + " => Rest for 17 minutes!!!")
             break
 
 def rest_countdown(n=17):
+    print ("INFO " + str(datetime.datetime.now()) + " => Init Rest")
     while n > 0:
         n = n - 1
         time.sleep(60)
         if n == 0:
             toaster = ToastNotifier()
             toaster.show_toast("The rest is over!!!","Let's continue working")
-            print("The rest is over!!!")
+            print("INFO " + str(datetime.datetime.now()) + " => The rest is over!!!")
             break
            
 while True:
